11330aa5de0365b25739d5fc832c3b678f4ad4a5
Kye <3532135+kY9i@users.noreply.github.com>
2022-08-29 11:26:49 +0800

    add keyType

--- jsonvalue.go
+++ jsonvalue.go
@@ -101,9 +101,10 @@ func (v *V) ValueType() ValueType {
 //
 // V 是 jsonvalue 的主类型，表示一个 JSON 值。
 type V struct {
+	keyType   uint
 	valueType ValueType
 
-	srcByte []byte
+	srcByte   []byte
 
 	num       num
 	valueStr  string
@@ -131,6 +132,19 @@ type children struct {
 
 	// As official json package supports caseless key accessing, I decide to do it as well
 	lowerCaseKeys map[string]map[string]struct{}
+}
+
+func (v *V) SetKeyType(t uint) {
+	if v.IsObject() {
+		v.keyType = t
+	}
+}
+
+func (v *V) GetKeyType() uint {
+	if v.IsObject() {
+		return v.keyType
+	}
+	return 0
 }
 
 func new(t ValueType) *V {
